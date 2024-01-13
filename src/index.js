1c915db896a6f5957ee0e404099b924e2769a3a6
WebghostX01 <atulegoodluck07@gmail.com>
2024-01-13 15:40:22 +0100

    version 1.0

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "@fontsource/poppins";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "./styles/GlobalStyles.css";
+import App from './App';
+import {store} from "./app/store"
+import { Provider } from 'react-redux'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
